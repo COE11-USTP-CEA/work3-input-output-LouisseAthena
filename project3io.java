38ed468384f3703ceb3d778483ee7020b7b5655e
LouisseAthena <athenalouisse32191@gmail.com>
2017-01-26 22:49:44 +0800

    Work 3

---
+++ project3io.java
@@ -0,0 +1,11 @@
+class project3io{
+
+
+    public static void main(String[] args) {
+        for(String c : args ){
+            System.out.println(c);
+        }
+        System.out.println("There are " +args.length + " arguments");
+        
+    }
+}
